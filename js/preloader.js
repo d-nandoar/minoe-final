9eaada548217c15a99c55053c9d440659f319abe
nando <249022704+d-nandoar@users.noreply.github.com>
2026-04-25 13:56:07 -0500

    commit 10, tamaños

--- js/preloader.js
+++ js/preloader.js
@@ -1,16 +1,22 @@
 window.addEventListener("load", () => {
-  // 5 segundos de espera para la coreografía lenta (logo/textos)
+  // Bloqueo preventivo redundante (por si acaso)
+  document.documentElement.style.overflow = "hidden";
+
   setTimeout(() => {
     document.body.classList.add("loaded");
 
-    // Tiempo que tarda el cierre verde en expandirse y retirarse
     setTimeout(() => {
+      // 1. Quitamos la clase de bloqueo
       document.body.classList.remove("no-scroll");
+
+      // 2. Liberamos el scroll explícitamente
+      document.documentElement.style.overflow = "auto";
+
       document.body.classList.add("loaded-complete");
 
       // ENVIAR SEÑAL AL HERO
       const eventoRevelado = new CustomEvent("paginaRevelada");
       window.dispatchEvent(eventoRevelado);
-    }, 1500); // Sincronizado con la transición de 2.2s del CSS
-  }, 8000);
+    }, 1500);
+  }, 5700);
 });
